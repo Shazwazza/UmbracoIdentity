fdc69e756864ac41d0594934762b50e0d039bffe
Shannon <sdeminick@gmail.com>
2014-07-15 12:28:10 +1000

    Adds build scripts and nuspecs

---
+++ build/Build-Release.ps1
@@ -0,0 +1,83 @@
+param (
+	[Parameter(Mandatory=$true)]
+	[ValidatePattern("^\d\.\d\.(?:\d\.\d$|\d$)")]
+	[string]
+	$ReleaseVersionNumber,
+	[Parameter(Mandatory=$true)]
+	[string]
+	[AllowEmptyString()]
+	$PreReleaseName
+)
+
+$PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path);
+$RepoRoot = (get-item $PSScriptFilePath).Directory.Parent.FullName;
+$SolutionRoot = Join-Path -Path $RepoRoot "src";
+
+#trace
+"Solution Root: $SolutionRoot"
+
+$MSBuild = "$Env:SYSTEMROOT\Microsoft.NET\Framework\v4.0.30319\msbuild.exe";
+
+# Make sure we don't have a release folder for this version already
+$BuildFolder = Join-Path -Path $RepoRoot -ChildPath "build";
+$ReleaseFolder = Join-Path -Path $BuildFolder -ChildPath "Releases\v$ReleaseVersionNumber$PreReleaseName";
+if ((Get-Item $ReleaseFolder -ErrorAction SilentlyContinue) -ne $null)
+{
+	Write-Warning "$ReleaseFolder already exists on your local machine. It will now be deleted."
+	Remove-Item $ReleaseFolder -Recurse
+}
+New-Item $ReleaseFolder -Type directory
+
+#trace
+"Release path: $ReleaseFolder"
+
+# Set the version number in SolutionInfo.cs
+$SolutionInfoPath = Join-Path -Path $SolutionRoot -ChildPath "SolutionInfo.cs"
+(gc -Path $SolutionInfoPath) `
+	-replace "(?<=Version\(`")[.\d]*(?=`"\))", $ReleaseVersionNumber |
+	sc -Path $SolutionInfoPath -Encoding UTF8
+(gc -Path $SolutionInfoPath) `
+	-replace "(?<=AssemblyInformationalVersion\(`")[.\w-]*(?=`"\))", "$ReleaseVersionNumber$PreReleaseName" |
+	sc -Path $SolutionInfoPath -Encoding UTF8
+# Set the copyright
+$Copyright = "Copyright © Shannon Deminick ".(Get-Date).year
+(gc -Path $SolutionInfoPath) `
+	-replace "(?<=AssemblyCopyright\(`")[.\w-]*(?=`"\))", $Copyright |
+	sc -Path $SolutionInfoPath -Encoding UTF8
+
+# Build the solution in release mode
+$SolutionPath = Join-Path -Path $SolutionRoot -ChildPath "UmbracoIdentity.sln";
+
+# clean sln for all deploys
+& $MSBuild "$SolutionPath" /p:Configuration=Release /maxcpucount /t:Clean
+if (-not $?)
+{
+	throw "The MSBuild process returned an error code."
+}
+
+#build
+& $MSBuild "$SolutionPath" /p:Configuration=Release /maxcpucount
+if (-not $?)
+{
+	throw "The MSBuild process returned an error code."
+}
+
+$include = @('UmbracoIdentity.dll','UmbracoIdentity.pdb')
+$CoreBinFolder = Join-Path -Path $SolutionRoot -ChildPath "UmbracoIdentity\bin\Release";
+Copy-Item "$CoreBinFolder\*.*" -Destination $ReleaseFolder -Include $include
+
+# COPY THE TRANSFORMS OVER
+# Copy-Item "$BuildFolder\nuget-transforms\Core\web.config.transform" -Destination (New-Item (Join-Path -Path $CoreFolder -ChildPath "nuget-transforms") -Type directory);
+
+# COPY OVER THE CORE NUSPEC AND BUILD THE NUGET PACKAGE
+$CoreNuSpecSource = Join-Path -Path $BuildFolder -ChildPath "UmbracoIdentity.nuspec";
+Copy-Item $CoreNuSpecSource -Destination $ReleaseFolder
+$CoreNuSpec = Join-Path -Path $ReleaseFolder -ChildPath "UmbracoIdentity.nuspec";
+$NuGet = Join-Path $SolutionRoot -ChildPath ".nuget\NuGet.exe"
+Write-Output "DEBUGGING: " $CoreNuSpec -OutputDirectory $ReleaseFolder -Version $ReleaseVersionNumber$PreReleaseName
+& $NuGet pack $CoreNuSpec -OutputDirectory $ReleaseFolder -Version $ReleaseVersionNumber$PreReleaseName
+
+""
+"Build $ReleaseVersionNumber$PreReleaseName is done!"
+"NuGet packages also created, so if you want to push them just run:"
+"  nuget push $CoreNuSpec"
---
+++ src/SolutionInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Security;
+
+[assembly: AssemblyCompany("Shannon Deminick")]
+[assembly: AssemblyCopyright("Copyright © Shannon Deminick 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
