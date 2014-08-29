4d57ccbe6c048a95507afc050b48848ac29164bb
Shannon <sdeminick@gmail.com>
2014-08-29 10:35:55 +1000

    Updates version

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -9,4 +9,4 @@ using System.Security;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0-beta")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
