de19a18d50a1955bf0350fb85346697805558603
Shannon <sdeminick@gmail.com>
2016-01-10 11:39:16 +0100

    Removes the SecurityStampValidator syntax from the custom startup class: - SecurityStampValidator Cause logout #25

--- src/UmbracoIdentity.Web/App_Code/UmbracoIdentityStartup.cs
+++ src/UmbracoIdentity.Web/App_Code/UmbracoIdentityStartup.cs
@@ -33,20 +33,9 @@ namespace UmbracoIdentity.Web
             // signed in user and to use a cookie to temporarily store information 
             // about a user logging in with a third party login provider 
             // Configure the sign in cookie
-            app.UseCookieAuthentication(new FrontEndCookieAuthenticationOptions
-            {
-                Provider = new CookieAuthenticationProvider
-                {
-                    // Enables the application to validate the security stamp when the user 
-                    // logs in. This is a security feature which is used when you 
-                    // change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator
-                        .OnValidateIdentity<UmbracoMembersUserManager<UmbracoApplicationMember>, UmbracoApplicationMember, int>(
-                            TimeSpan.FromMinutes(30),
-                            (manager, user) => user.GenerateUserIdentityAsync(manager),
-                            UmbracoIdentity.IdentityExtensions.GetUserId<int>)
-                }
-            });
+            app.UseCookieAuthentication(
+                //You can modify these options for any customizations you'd like
+                new FrontEndCookieAuthenticationOptions());
             
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
